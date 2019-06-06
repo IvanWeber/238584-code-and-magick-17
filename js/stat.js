2a6cea60ae332fe9c469d7c3b681ef3c9651eb1e
unknown <b24b24testing.ru>
2019-06-06 04:28:09 +0300

    Добавлена гистограмма с результатами игроков рассчитанная с помощью пропорций, осталось доработать цвет и вывод времени

--- js/stat.js
+++ js/stat.js
@@ -2,30 +2,44 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var SHADOW_GAP = 10;
+var Y_GAP = 45;
+var X_GAP = 10;
+var FONT_GAP = 20;
+var BAR_CHART_GAP = 200;
+var BAR_CHART_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
+var PLAYER_BAR_COLOR = 'rgba(255, 0, 0, 1)';
+var PLAYERS_BAR_COLOR = 'rgba(0,0,255,' + Math.random() + ')';
+var players = ['Вы', 'Иван', 'Юлия', 'Анна'];
+var times = [2500, 3000, 2000, 6000];
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 window.renderStatistics = function (ctx) {
-  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, 100, 10, '#fff');
+  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.font = '16px "PT Mono" sans-serif';
   ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', 110, 55);
-  ctx.fillText('Список результатов:', 110, 75);
+  ctx.fillText('Ура вы победили!', CLOUD_X + X_GAP, CLOUD_Y + Y_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + X_GAP, CLOUD_Y + Y_GAP + FONT_GAP);
 
-  ctx.fillText('Вы', 110, 95);
-  ctx.fillRect(160, 79, 430, 20);
+  ctx.fillText(players[0], CLOUD_X + X_GAP, CLOUD_Y + Y_GAP + FONT_GAP + BAR_CHART_GAP);
+  ctx.fillRect(CLOUD_X + X_GAP, CLOUD_Y + Y_GAP + 2 * FONT_GAP + BAR_CHART_HEIGHT - Math.round(times[0] * BAR_CHART_HEIGHT / Math.max.apply(null, times)), BAR_WIDTH, Math.round(times[0] * BAR_CHART_HEIGHT / Math.max.apply(null, times)));
 
-  ctx.fillText('Иван', 110, 125);
-  ctx.fillRect(160, 109, 430, 20);
+  ctx.fillText(players[1], CLOUD_X + X_GAP + BAR_WIDTH + BAR_GAP, CLOUD_Y + Y_GAP + FONT_GAP + BAR_CHART_GAP);
+  ctx.fillRect(CLOUD_X + X_GAP + BAR_WIDTH + BAR_GAP, CLOUD_Y + Y_GAP + 2 * FONT_GAP + BAR_CHART_HEIGHT - Math.round(times[1] * BAR_CHART_HEIGHT / Math.max.apply(null, times)), BAR_WIDTH, Math.round(times[1] * BAR_CHART_HEIGHT / Math.max.apply(null, times)));
 
-  ctx.fillText('Юлия', 110, 155);
-  ctx.fillRect(160, 139, 430, 20);
+  ctx.fillText(players[2], CLOUD_X + X_GAP + 2 * (BAR_WIDTH + BAR_GAP), CLOUD_Y + Y_GAP + FONT_GAP + BAR_CHART_GAP);
+  ctx.fillRect(CLOUD_X + X_GAP + 2 * (BAR_WIDTH + BAR_GAP), CLOUD_Y + Y_GAP + 2 * FONT_GAP + BAR_CHART_HEIGHT - Math.round(times[2] * BAR_CHART_HEIGHT / Math.max.apply(null, times)), BAR_WIDTH, Math.round(times[2] * BAR_CHART_HEIGHT / Math.max.apply(null, times)));
 
-  ctx.fillText('Анна', 110, 185);
-  ctx.fillRect(160, 169, 430, 20);
+  ctx.fillText(players[3], CLOUD_X + X_GAP + 3 * (BAR_WIDTH + BAR_GAP), CLOUD_Y + Y_GAP + FONT_GAP + BAR_CHART_GAP);
+  ctx.fillRect(CLOUD_X + X_GAP + 3 * (BAR_WIDTH + BAR_GAP), CLOUD_Y + Y_GAP + 2 * FONT_GAP + BAR_CHART_HEIGHT - Math.round(times[3] * BAR_CHART_HEIGHT / Math.max.apply(null, times)), BAR_WIDTH, Math.round(times[3] * BAR_CHART_HEIGHT / Math.max.apply(null, times)));
 };
