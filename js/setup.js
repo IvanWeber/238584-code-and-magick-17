f3bb84164ef3a5f811833fe6f1dee119c8f518a9
unknown <b24b24testing.ru>
2019-06-11 08:05:25 +0300

    Добавлена функция, возвращающая случайное целое число в заданном диапазоне, две константы с массивами имён и фамилий, а также функция, возвращающая случайное имя, сформированное из массивов, упомянутых выше

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,42 @@
 'use strict';
 
+var getRandomInt = function (min, max) {
+  var randomInt = Math.round(Math.random() * (max - min));
+  return randomInt;
+};
+
 var showSetupBlock = function () {
   var setupBlock = document.querySelector('.setup');
   setupBlock.classList.remove('hidden');
 };
 
 showSetupBlock();
+
+var FIRST_NAME = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+var LAST_NAME = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+
+var getName = function (firstName, lastName) {
+  var name = firstName[getRandomInt(0, firstName.length - 1)] + ' ' + lastName[getRandomInt(0, lastName.length - 1)];
+  return name;
+};
+
+console.log(getName(FIRST_NAME, LAST_NAME));
