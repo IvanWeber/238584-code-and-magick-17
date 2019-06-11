8874df07f3fbfb4b89cfb8af835d5d943628012b
unknown <b24b24testing.ru>
2019-06-11 08:15:25 +0300

    Добавлены константы с массивами цветом мантии и цветов глаз, а также функция принимающая массив и возвращающая случайный элемент этого массива. Заданы переменные, перечисленные во втором пункте задачи

--- js/setup.js
+++ js/setup.js
@@ -34,9 +34,37 @@ var LAST_NAME = [
   'Ирвинг'
 ];
 
+var COAT_COLOR = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+var EYES_COLOR = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
 var getName = function (firstName, lastName) {
   var name = firstName[getRandomInt(0, firstName.length - 1)] + ' ' + lastName[getRandomInt(0, lastName.length - 1)];
   return name;
 };
 
-console.log(getName(FIRST_NAME, LAST_NAME));
+var getRandomElementFromArray = function (someArray) {
+  var RandomElementFromArray = someArray[getRandomInt(0, someArray.length - 1)];
+  return RandomElementFromArray;
+};
+
+var name = getName(FIRST_NAME, LAST_NAME);
+var coatColor = getRandomElementFromArray(COAT_COLOR);
+var eyesColor = getRandomElementFromArray(EYES_COLOR);
+
+// console.log(name);
+// console.log(coatColor);
+// console.log(eyesColor);
